e86835fde7f6eda16f972bb0b2c41f1fa8ec83cd
Nick Wesselman <nwe@sitecore.net>
2019-03-07 14:27:21 -0500

    [#4] WIP on header and footer implementation

---
+++ basic-company/project-per-module-unicorn/src/Feature/Navigation/website/Services/HeaderBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using BasicCompany.Feature.Navigation.Models;
+using Sitecore.Data.Items;
+using Sitecore.Abstractions;
+
+namespace BasicCompany.Feature.Navigation.Services
+{
+	public class HeaderBuilder : IHeaderBuilder
+	{
+		protected readonly BaseLinkManager LinkManager;
+
+		public HeaderBuilder(BaseLinkManager linkManager)
+		{
+			Debug.Assert(linkManager != null);
+			LinkManager = linkManager;
+		}
+
+		public Header GetHeader(Item contextItem)
+		{
+			Debug.Assert(contextItem != null);
+			var navigationRoot = contextItem.DescendsFrom(Templates.NavigationRoot.Id) ? contextItem :
+				contextItem.Axes.GetAncestors().LastOrDefault(x => x.DescendsFrom(Templates.NavigationRoot.Id));
+			if (navigationRoot == null)
+			{
+				return new Header();
+			}
+
+			return new Header
+			{
+				HomeItem = navigationRoot,
+				HomeUrl = LinkManager.GetItemUrl(navigationRoot),
+				NavigationItems = GetNavigationItems(navigationRoot, contextItem)
+			};
+		}
+
+		private IList<NavigationItem> GetNavigationItems(Item navigationRoot, Item contextItem)
+		{
+			// Collect home/root item and its children which are navigable
+			var items = new List<Item> { navigationRoot };
+			items.AddRange(navigationRoot.Children.Where(item => item.DescendsFrom(Templates.NavigationItem.Id)));
+
+			// Initialize with home/root page
+			var navigationItems = items.Select(item => new NavigationItem
+			{
+				Item = item,
+				Url = LinkManager.GetItemUrl(item),
+				IsActive = item.ID == contextItem.ID
+			}).ToList();
+
+			return navigationItems;
+		}
+	}
+}
---
+++ basic-company/project-per-module-unicorn/src/Feature/Navigation/website/ServicesConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace BasicCompany.Feature.Navigation
+{
+	public class ServicesConfigurator : IServicesConfigurator
+	{
+		public void Configure(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient<Controllers.NavigationController>();
+			serviceCollection.AddTransient<Services.IHeaderBuilder, Services.HeaderBuilder>();
+		}
+	}
+}

c5c3a5d6891f32dceb7abe5e63e7e4e9a76c6a21
Nick Wesselman <nwe@sitecore.net>
2019-04-30 13:53:41 -0400

    [#6] Products feature module for listing products

---
+++ basic-company/project-per-module-unicorn/src/Feature/Products/website/ServicesConfigurator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicCompany.Feature.Products
+{
+    public class ServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<Controllers.ProductsController>();
+            serviceCollection.AddTransient<Services.IProductRepository, Services.ProductRepository>();
+        }
+    }
+}

9d8c90f40850d751ecfb2e9ebc1b00c1f8ea739a
Nick Wesselman <nwe@sitecore.net>
2019-08-22 15:50:55 -0400

    [#35] extracted global settings into their own config

--- helix-basic-unicorn/build/settings.ps1
+++ helix-basic-unicorn/build/settings.ps1
@@ -1,86 +1,12 @@
 # Metadata for menuing
 $ExampleName = "Helix Basic - Unicorn"
 $ExampleDescription = "This example demonstrates basic Helix solution architecture using Unicorn for serialization and Helix Publishing Pipeline for builds."
-$ExampleSrcPath = (Get-Item "$PSScriptRoot\..\src").FullName
-$InstallScript = (Get-Item "$PSScriptRoot\install.ps1").FullName
-$UninstallScript = (Get-Item "$PSScriptRoot\uninstall.ps1").FullName
 
-# Solution parameters
+# URL / Install Prefix
 $SolutionPrefix = "helix-basic-unicorn"
-$SitePostFix = "dev.local"
-$webroot = "C:\inetpub\wwwroot"
 
+# Solution build parameters
 $BuildProject = "$PSScriptRoot\..\src\Deployment\Website\Website.csproj"
 $UnicornSecretConfig = "$PSScriptRoot\..\src\Foundation\Serialization\website\App_Config\Include\Unicorn.SharedSecret.config"
 
-$SitecoreVersion = "9.1.0 rev. 001564"
-$IdentityServerVersion = "2.0.0 rev. 00157"
-$InstallerVersion = "2.0.0"
-
-$SqlBuildVersion = "13.0.5026"
-$SqlFriendlyVersion = "2016 SP2"
-
-# Build config
-$ConfigPath = "$PSScriptRoot\config"
-$PrepareConfiguration = "prepare.json"
-$ExpandAssetsConfiguration = "expand-install-assets.json"
-$InstallConfiguration = "install.json"
-$BuildAndSyncConfiguration = "build-and-sync.json"
-
-# Assets and prerequisites
-$AssetsRoot = "$PSScriptRoot\..\..\install-assets"
-$AssetsPSRepository = "https://sitecore.myget.org/F/sc-powershell/api/v2/"
-$AssetsPSRepositoryName = "SitecoreGallery"
-
-# SQL Parameters
-$SqlServer = "."
-$SqlAdminUser = "sa"
-$SqlAdminPassword = "12345"
-
-# Solr Parameters
-$SolrUrl = "https://localhost:8983/solr"
-$SolrRoot = "c:\\solr"
-$SolrService = "Solr"
-
-# Other user-configurable install params
-$SitecoreAdminPassword = "b"
-$IdentityClientSecret = "SPDHZpF6g8EXq5F7C5EhPQdsC1UbvTU3"
-
-# User overrides before we calculate values
-if (Test-Path $PSScriptRoot\settings.user.ps1) {
-    . $PSScriptRoot\settings.user.ps1
-}
-
-#
-# CALCULATED SETTINGS
-# TODO: Can't override outside this file?
-#
-
-#License File
-$LicenseFile = "$AssetsRoot\license.xml"
-
-# Certificates
-$CertPath = Join-Path "$AssetsRoot" "Certificates"
-
-# XP0 zip -- will be expanded in place if needed
-$DownloadZip = "$AssetsRoot\Sitecore $SitecoreVersion (WDP XP0 packages).zip"
-$ConfigurationsZip = "XP0 Configuration files $SitecoreVersion.zip"
-
-# Sitecore Parameters
-$SitecorePackage = "$AssetsRoot\Sitecore $SitecoreVersion (OnPrem)_single.scwdp.zip"
-$SitecoreSiteName = "$SolutionPrefix.$SitePostFix"
-$SitecoreSiteUrl = "http://$SitecoreSiteName"
-$SitecoreSiteRoot = Join-Path $webroot -ChildPath $SitecoreSiteName
-
-# XConnect Parameters
-$XConnectPackage = "$AssetsRoot\Sitecore $SitecoreVersion (OnPrem)_xp0xconnect.scwdp.zip"
-$XConnectSiteName = "${SolutionPrefix}_xconnect.$SitePostFix"
-$XConnectSiteUrl = "https://$XConnectSiteName"
-$XConnectSiteRoot = Join-Path $webroot -ChildPath $XConnectSiteName
-
-# Identity Server Parameters
-$IdentityServerSiteName = "${SolutionPrefix}_IdentityServer.$SitePostFix"
-$IdentityServerUrl = "https://$IdentityServerName"
-$IdentityServerPackage = "$AssetsRoot\Sitecore.IdentityServer $IdentityServerVersion (OnPrem)_identityserver.scwdp.zip"
-$IdentityAllowedCorsOrigins = $SitecoreSiteUrl
-$IdentityServerSiteRoot = Join-Path $webroot -ChildPath $IdentityServerSiteName
+. $PSScriptRoot\..\..\settings.global.ps1 -ExampleBuildDirectory $PSScriptRoot
---
+++ settings.global.ps1
@@ -0,0 +1,78 @@
+# Always execute relative to calling settings
+param($ExampleBuildDirectory)
+
+$SitecoreVersion = "9.1.0 rev. 001564"
+$IdentityServerVersion = "2.0.0 rev. 00157"
+$InstallerVersion = "2.0.0"
+
+$SqlBuildVersion = "13.0.5026"
+$SqlFriendlyVersion = "2016 SP2"
+
+$SitePostFix = "dev.local"
+$webroot = "C:\inetpub\wwwroot"
+
+$ExampleSrcPath = (Get-Item "$ExampleBuildDirectory\..\src").FullName
+$InstallScript = (Get-Item "$ExampleBuildDirectory\install.ps1").FullName
+$UninstallScript = (Get-Item "$ExampleBuildDirectory\uninstall.ps1").FullName
+
+# Build config
+$ConfigPath = "$ExampleBuildDirectory\config"
+$PrepareConfiguration = "prepare.json"
+$ExpandAssetsConfiguration = "expand-install-assets.json"
+$InstallConfiguration = "install.json"
+$BuildAndSyncConfiguration = "build-and-sync.json"
+$AssetsRoot = "$PSScriptRoot\install-assets"
+
+# SQL Parameters
+$SqlServer = "."
+$SqlAdminUser = "sa"
+$SqlAdminPassword = "12345"
+
+# Solr Parameters
+$SolrUrl = "https://localhost:8983/solr"
+$SolrRoot = "c:\\solr"
+$SolrService = "Solr"
+
+# Other user-configurable install params
+$SitecoreAdminPassword = "b"
+$IdentityClientSecret = "SPDHZpF6g8EXq5F7C5EhPQdsC1UbvTU3"
+
+# User overrides before we calculate values
+if (Test-Path $PSScriptRoot\settings.user.ps1) {
+    . $PSScriptRoot\settings.user.ps1
+}
+
+
+#
+# CALCULATED SETTINGS
+# TODO: Can't override outside this file?
+#
+
+#License File
+$LicenseFile = "$AssetsRoot\license.xml"
+
+# Certificates
+$CertPath = Join-Path "$AssetsRoot" "Certificates"
+
+# XP0 zip -- will be expanded in place if needed
+$DownloadZip = "$AssetsRoot\Sitecore $SitecoreVersion (WDP XP0 packages).zip"
+$ConfigurationsZip = "XP0 Configuration files $SitecoreVersion.zip"
+
+# Sitecore Parameters
+$SitecorePackage = "$AssetsRoot\Sitecore $SitecoreVersion (OnPrem)_single.scwdp.zip"
+$SitecoreSiteName = "$SolutionPrefix.$SitePostFix"
+$SitecoreSiteUrl = "http://$SitecoreSiteName"
+$SitecoreSiteRoot = Join-Path $webroot -ChildPath $SitecoreSiteName
+
+# XConnect Parameters
+$XConnectPackage = "$AssetsRoot\Sitecore $SitecoreVersion (OnPrem)_xp0xconnect.scwdp.zip"
+$XConnectSiteName = "${SolutionPrefix}_xconnect.$SitePostFix"
+$XConnectSiteUrl = "https://$XConnectSiteName"
+$XConnectSiteRoot = Join-Path $webroot -ChildPath $XConnectSiteName
+
+# Identity Server Parameters
+$IdentityServerSiteName = "${SolutionPrefix}_IdentityServer.$SitePostFix"
+$IdentityServerUrl = "https://$IdentityServerName"
+$IdentityServerPackage = "$AssetsRoot\Sitecore.IdentityServer $IdentityServerVersion (OnPrem)_identityserver.scwdp.zip"
+$IdentityAllowedCorsOrigins = $SitecoreSiteUrl
+$IdentityServerSiteRoot = Join-Path $webroot -ChildPath $IdentityServerSiteName

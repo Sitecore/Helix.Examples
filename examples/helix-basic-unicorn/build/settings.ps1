cb2009a79b8909e026850c058ff0a0a372410231
Nick Wesselman <nwe@sitecore.net>
2019-10-04 15:22:45 -0400

    Helix Basic - Unicorn fixes related to #53

--- examples/helix-basic-unicorn/build/settings.ps1
+++ examples/helix-basic-unicorn/build/settings.ps1
@@ -1,14 +1,14 @@
 # Metadata for menuing
 $ExampleName = "Helix Basic - Unicorn"
 $ExampleDescription = "This example demonstrates basic Helix solution architecture using Unicorn for serialization and Helix Publishing Pipeline for builds."
-$ExampleUrl = "https://github.com/Sitecore/Helix.Examples/examples/helix-basic-unicorn"
+$ExampleUrl = "https://sitecore.github.io/Helix.Examples/examples.html"
 
 # URL / Install Prefix
 $SolutionPrefix = "helix-basic-unicorn"
 
 # Solution build parameters
 $SourceFolder = Resolve-Path "$PSScriptRoot\..\src"
-$BuildProject = "$SourceFolder\Deployment\Website\Website.csproj"
+$BuildProject = "$SourceFolder\Environment\Website\Website.csproj"
 $UnicornSecretConfig = "$SourceFolder\Foundation\Serialization\website\App_Config\Include\Unicorn.SharedSecret.config"
 
 . $PSScriptRoot\..\..\..\settings.global.ps1 -ExampleBuildDirectory $PSScriptRoot

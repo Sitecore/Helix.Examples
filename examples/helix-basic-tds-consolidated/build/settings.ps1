5cbd4cec7ccb23d25072e8cabe787d889194c045
Nick Wesselman <nwe@sitecore.net>
2019-10-14 16:01:38 -0400

    [#61] initial implementation of Basic Company with consolidated project structure and TDS

---
+++ examples/helix-basic-tds-consolidated/build/settings.ps1
@@ -0,0 +1,10 @@
+$ExampleName = "Helix Basic - TDS - Consolidated"
+$ExampleDescription = "This example demonstrates basic Helix solution architecture, with a simplified Visual Studio structure, using TDS for serialization and builds."
+$ExampleUrl = "https://sitecore.github.io/Helix.Examples/examples.html"
+$SolutionPrefix = "helix-basic-tds-consolidated"
+
+# Solution build parameters
+$SourceFolder = Resolve-Path "$PSScriptRoot\..\src"
+$BuildProject = "$SourceFolder\..\BasicCompany.sln"
+
+. $PSScriptRoot\..\..\..\settings.global.ps1 -ExampleBuildDirectory $PSScriptRoot
---
+++ examples/helix-basic-tds-consolidated/src/BasicCompany.Web/Foundation/FieldRendering/README.md
@@ -0,0 +1,7 @@
+# Field Rendering Module
+
+The domain of this Foundation module is extensions/helpers and
+customizations related to Sitecore field rendering, which are used
+across multiple Feature modules. This includes:
+* Extension methods for `@Html.Sitecore()` to simplify rendering of
+fields by ID, and other common field rendering needs

989b0b8576b4372ad595cdfe6a81cc1417ce563c
Adam Brauer <adab@sitecore.net>
2020-07-21 10:18:20 -0500

    ASP.NET Core example added.

---
+++ examples/helix-basic-aspnetcore/src/Feature/Navigation/rendering/Models/LogoLink.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.AspNet.RenderingEngine.Binding.Attributes;
+using Sitecore.LayoutService.Client.Response.Model.Fields;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCompany.Feature.Navigation.Models
+{
+    public class LogoLink
+    {
+        public TextField NavigationTitle { get; set; }
+
+        public ImageField HeaderLogo { get; set; }
+    }
+}
---
+++ examples/helix-basic-aspnetcore/src/Feature/Navigation/sitecore/Models/NavigationItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace BasicCompany.Feature.Navigation.Models
+{
+    public class NavigationItem
+    {
+        public Item Item { get; set; }
+        public string Url { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

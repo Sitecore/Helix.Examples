a0d397a2e5c73ba911b40ddb914b3b578cb1bf86
Nick Wesselman <nwe@sitecore.net>
2020-07-29 14:16:24 -0400

    Sitecore -> Platform in headless Helix project per community feedback

--- examples/helix-basic-aspnetcore/src/Feature/Navigation/platform/Properties/AssemblyInfo.cs
+++ examples/helix-basic-aspnetcore/src/Feature/Navigation/platform/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BasicCompany.Feature.Navigation.Sitecore")]
+[assembly: AssemblyTitle("BasicCompany.Feature.Navigation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BasicCompany.Feature.Navigation.Sitecore")]
+[assembly: AssemblyProduct("BasicCompany.Feature.Navigation")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

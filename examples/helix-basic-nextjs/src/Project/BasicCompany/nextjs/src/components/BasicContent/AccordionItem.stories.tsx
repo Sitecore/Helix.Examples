2d58aca859aeeb46d828286314a8d5f2b6019ead
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2021-02-04 13:41:56 -0500

    implemented Accordion Item

---
+++ examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/components/BasicContent/AccordionItem.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import AccordionItem, { AccordionItemProps } from './AccordionItem';
+import { LayoutServiceContext, SitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
+
+export default {
+    title: 'Basic Content/Accordion Item',
+    component: AccordionItem,
+} as Meta;
+
+const Template: Story<AccordionItemProps> = (args) => <AccordionItem {...args} />;
+
+const withFields = (fields: {
+    title: string
+    content: string
+}): AccordionItemProps => {
+    return {
+        fields: {
+            Title: {
+                value: fields.title
+            },
+            Content: {
+                value: fields.content
+            }
+        }
+    };
+};
+
+export const WithAllFields = Template.bind({});
+WithAllFields.args = withFields({
+    title: 'Ut adipisicing velit qui labore duis fugiat',
+    content: `<p>Esse commodo mollit magna consequat proident ea deserunt culpa dolor ut occaecat aute reprehenderit.
+    Est nulla ullamco <strong>eiusmod</strong> consequat.</p>
+    <ul>
+        <li>Anim eu sint qui cupidatat commodo tempor voluptate deserunt proident commodo</li>
+        <li>Labore quis ut ipsum Lorem.</li>
+        <li>Voluptate eiusmod occaecat voluptate proident dolor.</li>
+    </ul>
+    <p>Aute ipsum. Commodo ullamco consequat anim velit. Occaecat est Lorem nostrud deserunt ipsum deserunt culpa cillum.
+    Tempor id non culpa deserunt anim ipsum.</p>`
+});
+
+export const Editing = Template.bind({});
+Editing.decorators = [(Story) =>
+    <SitecoreContext componentFactory={(_componentName: string) => null} context={{ pageEditing: true }}>
+        <Story />
+    </SitecoreContext>
+];
+Editing.args = withFields({
+    title: 'You cannot close this accordion',
+    content: 'Dolore duis veniam enim non nostrud mollit id elit et cupidatat.'
+});
---
+++ examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/components/BasicContent/AccordionItem.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Text, RichText, Field, useSitecoreContext, LayoutServiceContext } from '@sitecore-jss/sitecore-jss-nextjs';
+
+export type AccordionItemProps = {
+    fields: {
+        Title: Field<string>,
+        Content: Field<string>
+    }
+};
+
+const AccordionItem = ({ fields }: AccordionItemProps): JSX.Element => {
+    // when editing, default to open, and keep open
+    const { sitecoreContext } = useSitecoreContext<LayoutServiceContext>();
+    const isEditing = sitecoreContext && sitecoreContext.pageEditing;
+
+    const [isActive, setActiveState] = useState(isEditing);
+
+    const toggleAccordion = () => {
+        setActiveState(isEditing || !isActive);
+    }
+
+    return (
+        <div className={`accordion-item ${isActive ? 'is-active' : ''}`}>
+            <div className="accordion-header">
+                <Text field={fields.Title} tag="h3" />
+                <button onClick={toggleAccordion}></button>
+            </div>
+            <div className="accordion-body">
+                <RichText field={fields.Content} />
+            </div>
+        </div>
+    );
+};
+
+export default AccordionItem;

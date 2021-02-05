60bb27bd358ea11d32d70b1b4496387a0f03a85c
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2021-02-05 13:38:54 -0500

    EE tweaks

--- examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/components/BasicContent/Accordion.tsx
+++ examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/components/BasicContent/Accordion.tsx
@@ -12,14 +12,16 @@ export type AccordionProps = {
 
 const Accordion = ({ rendering }: AccordionProps): JSX.Element => {
   const isEditing = useSitecoreContext<LayoutServiceContext>()?.sitecoreContext?.pageEditing;
+  const placeholder = rendering?.placeholders?.accordion;
+  const isEmpty = placeholder?.filter((x) => (x as ComponentRendering).componentName).length == 0;
   return (
     <div className="container accordion">
-      <Placeholder name="accordion" rendering={rendering} />
-      {rendering?.placeholders?.accordion && isEditing && (
+      {isEmpty && isEditing && (
         <p style={{ padding: '20px', border: '1px dotted black' }}>
           <em>Add new accordion components here.</em>
         </p>
       )}
+      <Placeholder name="accordion" rendering={rendering} />
     </div>
   );
 };
--- examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/components/BasicContent/PromoContainer.tsx
+++ examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/components/BasicContent/PromoContainer.tsx
@@ -12,15 +12,20 @@ export type PromoContainerProps = {
 
 const PromoContainer = ({ rendering }: PromoContainerProps): JSX.Element => {
   const isEditing = useSitecoreContext<LayoutServiceContext>()?.sitecoreContext?.pageEditing;
+  const placeholder = rendering?.placeholders?.promos;
+  const isEmpty = placeholder?.filter((x) => (x as ComponentRendering).componentName).length == 0;
   return (
     <div className="container">
       <div className="columns">
-        <Placeholder name="promos" rendering={rendering} />
-        {rendering?.placeholders?.promos && isEditing && (
-          <p style={{ padding: '20px', border: '1px dotted black' }}>
+        {isEmpty && isEditing && (
+          <div
+            className="column"
+            style={{ padding: '20px', border: '1px dotted black', textAlign: 'center' }}
+          >
             <em>Add new promo components here.</em>
-          </p>
+          </div>
         )}
+        <Placeholder name="promos" rendering={rendering} />
       </div>
     </div>
   );

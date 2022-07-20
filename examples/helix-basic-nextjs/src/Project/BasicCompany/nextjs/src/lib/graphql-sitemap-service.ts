a9347cfb845ccaff51645f92bfe48c66a253ca36
miguel.minoldo <miguel.minoldo@valtech.com>
2022-07-20 08:57:51 +0200

    Fixed the graphql sitemap service by adding the missing apikey and sitename to the constructor initialization.

--- examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/lib/graphql-sitemap-service.ts
+++ examples/helix-basic-nextjs/src/Project/BasicCompany/nextjs/src/lib/graphql-sitemap-service.ts
@@ -3,6 +3,8 @@ import config from 'temp/config';
 
 const graphQLSitemapService = new GraphQLSitemapService({
   endpoint: config.graphqlEndpoint,
+  apiKey: config.sitecoreApiKey,
+  siteName: config.jssAppName,
 });
 
 export { graphQLSitemapService };
